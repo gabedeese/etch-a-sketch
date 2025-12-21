0f3e5ebbaf3afe05f2e4ec839dc0f3c3a4deedf2
gabedeese <182055125+gabedeese@users.noreply.github.com>
2025-12-21 05:45:54 -0500

    Add script and stylesheet

---
+++ script.js
@@ -0,0 +1,9 @@
+// Make a grid (n x n) the size of what the user inputs
+function makeGrid(gridSize) {
+  const blockSize = Math.sqrt(gridSize);
+  for (i = 0; i < gridSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("block");
+    row
+  }
+}
